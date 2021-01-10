b8ceb9a1fc31c29339533a8956f16604013a0e15
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-01-10 20:45:57 +0530

    Show request headers & params in app layout

--- lib/microlog_web/views/layout_view.ex
+++ lib/microlog_web/views/layout_view.ex
@@ -7,4 +7,21 @@ defmodule MicroLogWeb.LayoutView do
       _ -> "Phoenix Sample App"
     end
   end
+
+  def headers(conn) do
+    conn.req_headers
+    |> Enum.filter(fn {k, _} -> k != "cookie" end)
+    |> Enum.map(fn {k, v} -> {transform_header_key(k), v} end)
+  end
+
+  defp transform_header_key("dnt") do
+    "DNT"
+  end
+
+  defp transform_header_key(string) do
+    string
+    |> String.split("-")
+    |> Enum.map(fn s -> String.capitalize(s) end)
+    |> Enum.join("-")
+  end
 end
