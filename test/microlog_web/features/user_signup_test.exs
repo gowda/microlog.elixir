d681ffc9b22007fdc0fdece3f4fd825ab6933c60
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-05-08 13:12:35 +0530

    Add more tests for signup form

--- test/microlog_web/features/user_signup_test.exs
+++ test/microlog_web/features/user_signup_test.exs
@@ -3,7 +3,7 @@ defmodule MicroLog.UserSignupTest do
   use Wallaby.Feature
   # use Wallaby.DSL
 
-  feature "signup form has fields", %{session: session} do
+  feature "form has fields", %{session: session} do
     session
     |> visit("/")
     |> click(Query.link("Sign up now!"))
@@ -14,7 +14,7 @@ defmodule MicroLog.UserSignupTest do
     |> assert_has(Query.button("Create my account"))
   end
 
-  feature "submitting blank signup form shows errors", %{session: session} do
+  feature "submitting blank form shows errors", %{session: session} do
     session
     |> visit("/signup")
     |> click(Query.button("Create my account"))
@@ -24,12 +24,34 @@ defmodule MicroLog.UserSignupTest do
     |> assert_has(Query.text("Password can't be blank"))
   end
 
-  feature "submitting signup form with invalid email", %{session: session} do
+  feature "submitting form with invalid email", %{session: session} do
     session
     |> visit("/signup")
     |> fill_in(Query.text_field("user_email"), with: "invalid.email")
     |> click(Query.button("Create my account"))
     |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
     |> assert_has(Query.text("Email has invalid format"))
+  end
+
+  feature "submitting form with password mismatch", %{session: session} do
+    session
+    |> visit("/signup")
+    |> fill_in(Query.text_field("user_email"), with: "test@example.org")
+    |> fill_in(Query.text_field("user_name"), with: "Test user")
+    |> fill_in(Query.text_field("user_password"), with: "password")
+    |> click(Query.button("Create my account"))
+    |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
+    |> assert_has(Query.text("Password confirmation does not match"))
+  end
+
+  feature "submitting valid form", %{session: session} do
+    session
+    |> visit("/signup")
+    |> fill_in(Query.text_field("user_email"), with: "test@example.org")
+    |> fill_in(Query.text_field("user_name"), with: "Test user")
+    |> fill_in(Query.text_field("user_password"), with: "password")
+    |> fill_in(Query.text_field("user_password_confirmation"), with: "password")
+    |> click(Query.button("Create my account"))
+    |> assert_has(Query.text("Welcome to Phoenix Sample App!"))
   end
 end
