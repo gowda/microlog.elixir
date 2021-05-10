3fe6921661dfd0b4f3c92a1c66e2ae567bd55289
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-05-10 08:05:42 +0530

    Use 'setup' to create user in features

--- test/microlog_web/features/user_profile_edit_test.exs
+++ test/microlog_web/features/user_profile_edit_test.exs
@@ -2,7 +2,72 @@ defmodule MicroLog.UserProfileEditTest do
   use ExUnit.Case, async: true
   use Wallaby.Feature
 
-  def fixture(:user) do
+  describe "profile" do
+    setup [:create_user]
+
+    feature "redirects to login page when not logged in", %{session: session, user: user} do
+      session
+      |> visit("/users/#{user.id}/edit")
+      |> assert_has(Query.text("Please login to access this page"))
+      |> assert_has(Query.fillable_field("email"))
+      |> assert_has(Query.fillable_field("password"))
+      |> assert_has(Query.button("Log in"))
+    end
+
+    feature "form has fields", %{session: session, user: user} do
+      session
+      |> visit("/login")
+      |> fill_in(Query.fillable_field("email"), with: user.email)
+      |> fill_in(Query.fillable_field("password"), with: "password")
+      |> click(Query.button("Log in"))
+      |> click(Query.link("Settings"))
+      |> assert_has(Query.text("Update your profile"))
+      |> assert_has(Query.fillable_field("user[name]"))
+      |> assert_has(Query.fillable_field("user[email]"))
+      |> assert_has(Query.button("Save changes"))
+    end
+
+    feature "submitting blank form shows errors", %{session: session, user: user} do
+      session
+      |> login(user)
+      |> click(Query.link("Settings"))
+      |> clear(Query.fillable_field("user[name]"))
+      |> clear(Query.fillable_field("user[email]"))
+      |> click(Query.button("Save changes"))
+      |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
+      |> assert_has(Query.text("Name can't be blank"))
+    end
+
+    feature "submitting form with changed email shows errors", %{session: session, user: user} do
+      session
+      |> login(user)
+      |> click(Query.link("Settings"))
+      |> clear(Query.fillable_field("user[email]"))
+      |> fill_in(Query.fillable_field("user[email]"), with: "updated.user@example.org")
+      |> click(Query.button("Save changes"))
+      |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
+      |> assert_has(Query.text("Email cannot be updated"))
+    end
+
+    feature "submitting form with valid name", %{session: session, user: user} do
+      session
+      |> login(user)
+      |> click(Query.link("Settings"))
+      |> fill_in(Query.text_field("user[name]"), with: "Updated test user name")
+      |> click(Query.button("Save changes"))
+      |> assert_has(Query.text("Profile updated successfully."))
+    end
+
+    defp login(session, user) do
+      session
+      |> visit("/login")
+      |> fill_in(Query.fillable_field("email"), with: user.email)
+      |> fill_in(Query.fillable_field("password"), with: "password")
+      |> click(Query.button("Log in"))
+    end
+  end
+
+  defp create_user(_) do
     {:ok, user} =
       MicroLog.Accounts.create_user(%{
         email: "user@example.org",
@@ -11,77 +76,6 @@ defmodule MicroLog.UserProfileEditTest do
         password_confirmation: "password"
       })
 
-    user
-  end
-
-  feature "redirects to login page when not logged in", %{session: session} do
-    user = fixture(:user)
-
-    session
-    |> visit("/users/#{user.id}/edit")
-    |> assert_has(Query.text("Please login to access this page"))
-    |> assert_has(Query.fillable_field("email"))
-    |> assert_has(Query.fillable_field("password"))
-    |> assert_has(Query.button("Log in"))
-  end
-
-  feature "form has fields", %{session: session} do
-    user = fixture(:user)
-
-    session
-    |> visit("/login")
-    |> fill_in(Query.fillable_field("email"), with: user.email)
-    |> fill_in(Query.fillable_field("password"), with: "password")
-    |> click(Query.button("Log in"))
-    |> click(Query.link("Settings"))
-    |> assert_has(Query.text("Update your profile"))
-    |> assert_has(Query.fillable_field("user[name]"))
-    |> assert_has(Query.fillable_field("user[email]"))
-    |> assert_has(Query.button("Save changes"))
-  end
-
-  feature "submitting blank form shows errors", %{session: session} do
-    user = fixture(:user)
-
-    session
-    |> login(user)
-    |> click(Query.link("Settings"))
-    |> clear(Query.fillable_field("user[name]"))
-    |> clear(Query.fillable_field("user[email]"))
-    |> click(Query.button("Save changes"))
-    |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
-    |> assert_has(Query.text("Name can't be blank"))
-  end
-
-  feature "submitting form with changed email shows errors", %{session: session} do
-    user = fixture(:user)
-
-    session
-    |> login(user)
-    |> click(Query.link("Settings"))
-    |> clear(Query.fillable_field("user[email]"))
-    |> fill_in(Query.fillable_field("user[email]"), with: "updated.user@example.org")
-    |> click(Query.button("Save changes"))
-    |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
-    |> assert_has(Query.text("Email cannot be updated"))
-  end
-
-  feature "submitting form with valid name", %{session: session} do
-    user = fixture(:user)
-
-    session
-    |> login(user)
-    |> click(Query.link("Settings"))
-    |> fill_in(Query.text_field("user[name]"), with: "Updated test user name")
-    |> click(Query.button("Save changes"))
-    |> assert_has(Query.text("Profile updated successfully."))
-  end
-
-  def login(session, user) do
-    session
-    |> visit("/login")
-    |> fill_in(Query.fillable_field("email"), with: user.email)
-    |> fill_in(Query.fillable_field("password"), with: "password")
-    |> click(Query.button("Log in"))
+    %{user: user}
   end
 end
--- test/microlog_web/features/user_signup_test.exs
+++ test/microlog_web/features/user_signup_test.exs
@@ -2,55 +2,57 @@ defmodule MicroLog.UserSignupTest do
   use ExUnit.Case, async: true
   use Wallaby.Feature
 
-  feature "form has fields", %{session: session} do
-    session
-    |> visit("/")
-    |> click(Query.link("Sign up now!"))
-    |> assert_has(Query.fillable_field("user[name]"))
-    |> assert_has(Query.fillable_field("user[email]"))
-    |> assert_has(Query.fillable_field("user[password]"))
-    |> assert_has(Query.fillable_field("user[password_confirmation]"))
-    |> assert_has(Query.button("Create my account"))
-  end
+  describe "signup" do
+    feature "form has fields", %{session: session} do
+      session
+      |> visit("/")
+      |> click(Query.link("Sign up now!"))
+      |> assert_has(Query.fillable_field("user[name]"))
+      |> assert_has(Query.fillable_field("user[email]"))
+      |> assert_has(Query.fillable_field("user[password]"))
+      |> assert_has(Query.fillable_field("user[password_confirmation]"))
+      |> assert_has(Query.button("Create my account"))
+    end
 
-  feature "submitting blank form shows errors", %{session: session} do
-    session
-    |> visit("/signup")
-    |> click(Query.button("Create my account"))
-    |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
-    |> assert_has(Query.text("Name can't be blank"))
-    |> assert_has(Query.text("Email can't be blank"))
-    |> assert_has(Query.text("Password can't be blank"))
-  end
+    feature "submitting blank form shows errors", %{session: session} do
+      session
+      |> visit("/signup")
+      |> click(Query.button("Create my account"))
+      |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
+      |> assert_has(Query.text("Name can't be blank"))
+      |> assert_has(Query.text("Email can't be blank"))
+      |> assert_has(Query.text("Password can't be blank"))
+    end
 
-  feature "submitting form with invalid email", %{session: session} do
-    session
-    |> visit("/signup")
-    |> fill_in(Query.text_field("user_email"), with: "invalid.email")
-    |> click(Query.button("Create my account"))
-    |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
-    |> assert_has(Query.text("Email has invalid format"))
-  end
+    feature "submitting form with invalid email", %{session: session} do
+      session
+      |> visit("/signup")
+      |> fill_in(Query.text_field("user_email"), with: "invalid.email")
+      |> click(Query.button("Create my account"))
+      |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
+      |> assert_has(Query.text("Email has invalid format"))
+    end
 
-  feature "submitting form with password mismatch", %{session: session} do
-    session
-    |> visit("/signup")
-    |> fill_in(Query.text_field("user_email"), with: "test@example.org")
-    |> fill_in(Query.text_field("user_name"), with: "Test user")
-    |> fill_in(Query.text_field("user_password"), with: "password")
-    |> click(Query.button("Create my account"))
-    |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
-    |> assert_has(Query.text("Password confirmation does not match"))
-  end
+    feature "submitting form with password mismatch", %{session: session} do
+      session
+      |> visit("/signup")
+      |> fill_in(Query.text_field("user_email"), with: "test@example.org")
+      |> fill_in(Query.text_field("user_name"), with: "Test user")
+      |> fill_in(Query.text_field("user_password"), with: "password")
+      |> click(Query.button("Create my account"))
+      |> assert_has(Query.text("Oops, something went wrong! Please check the errors below."))
+      |> assert_has(Query.text("Password confirmation does not match"))
+    end
 
-  feature "submitting valid form", %{session: session} do
-    session
-    |> visit("/signup")
-    |> fill_in(Query.text_field("user_email"), with: "test@example.org")
-    |> fill_in(Query.text_field("user_name"), with: "Test user")
-    |> fill_in(Query.text_field("user_password"), with: "password")
-    |> fill_in(Query.text_field("user_password_confirmation"), with: "password")
-    |> click(Query.button("Create my account"))
-    |> assert_has(Query.text("Welcome to Phoenix Sample App!"))
+    feature "submitting valid form", %{session: session} do
+      session
+      |> visit("/signup")
+      |> fill_in(Query.text_field("user_email"), with: "test@example.org")
+      |> fill_in(Query.text_field("user_name"), with: "Test user")
+      |> fill_in(Query.text_field("user_password"), with: "password")
+      |> fill_in(Query.text_field("user_password_confirmation"), with: "password")
+      |> click(Query.button("Create my account"))
+      |> assert_has(Query.text("Welcome to Phoenix Sample App!"))
+    end
   end
 end
